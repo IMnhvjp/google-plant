87aad6ea65b308e425cf8ce8bcd96b8d17ac120d
IMnhvjp <levanminh19102003@gmail.com>
2024-11-27 16:28:12 +0700

    edit

---
+++ src/app/search/page.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from "next/navigation";
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
+import Image from "next/image";
+
+export default function Search() {
+
+//   const SearchMutaion = useMutation( {
+//     mutationFn: async () => {
+
+//     },
+//     onSuccess: () => {
+
+//     },
+//     onError: () => {
+
+//     }
+//   })
+
+  return (
+    <>
+        <div>Hello</div>
+    </>
+  )
+}
